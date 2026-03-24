554e2ca437ce49a1b22fdbce46cf26c5d610e0de
Nicolas Jovanny9.99 <169153222+Nicoco009@users.noreply.github.com>
2026-03-24 19:19:03 -0400

    Create bug.js

---
+++ utils/bug.js
@@ -0,0 +1,24 @@
+async function bug(message, client, texts, num) {
+    try {
+        const remoteJid = message.key?.remoteJid;
+        await client.sendMessage(remoteJid, {
+            image: { url: `database/${num}.jpg` },
+            caption: `> ${texts}`,
+            contextInfo: {
+                externalAdReply: {
+                    title: "Join Our WhatsApp Channel",
+                    body: " Nicolas Jovanny ",
+                    mediaType: 1,
+                    thumbnailUrl: `https://whatsapp.com/channel/0029Vb75YyQ72WTvpMm0TG3p`,
+                    renderLargerThumbnail: false,
+                    mediaUrl: `${num}.jpg`,
+                    sourceUrl: `${num}.jpg`
+                }
+            }
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export default bug;
